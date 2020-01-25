d2a4f6cb9272754be32f111c081dbcaa09ced00e
Michel <michelschep@gmail.com>
2020-01-25 17:08:57 +0100

    Playing with 3D

--- basics/basics.js
+++ basics/basics.js
@@ -1,8 +1,14 @@
+let detailY;
+// slide to see how detailY works
 function setup() {
-  size(500, 500)
+  createCanvas(100, 100, WEBGL);
+  detailY = createSlider(3, 16, 3);
+  detailY.position(10, height + 5);
+  detailY.style('width', '80px');
 }
 
-
 function draw() {
-
+  background(205, 105, 94);
+  rotateY(millis() / 1000);
+  sphere(40, 16, detailY.value());
 }
