ce427a04d8c5c09a2ee7e3e3074523ff1233e73a
Michel <michelschep@gmail.com>
2020-01-26 13:54:34 +0100

    Add velocity

--- spike_rankings_solar_system/spike_rankings_solar_system.js
+++ spike_rankings_solar_system/spike_rankings_solar_system.js
@@ -1,6 +1,16 @@
 let Player = function(position) {
   this.position = position;
   this.mass = 10;
+  this.velocity = createVector(0.03, 0.1);
+
+  this.update = function() {
+    this.position.add(this.velocity); 
+  }
+
+  this.draw = function() {
+       fill(255, 0, 0);
+       ellipse(this.position.x, this.position.y, this.mass, this.mass);
+  }
 };
 
 
@@ -12,9 +22,13 @@ function preload() {
 
   //players = {list: [ { name: "Michel"}, { name: "Geale"} ]};
   //players = [ new Player(createVector(random(width), random(height)))];
-  var player = new Player(createVector(100, 100));
-  player.mass = 50;
-  players = [ player ];
+  var player1 = new Player(createVector(100, 100));
+  player1.mass = 50;
+
+  var player2 = new Player(createVector(200, 200));
+  player2.mass = 30;
+
+  players = [ player1, player2 ];
 }
 
 function setup() {
@@ -22,13 +36,11 @@ function setup() {
     background(100);
 }
 
-
 function draw() {
    background(100);
    for (var index=0; index<players.length; ++index) {
        console.log(players[index].position);
-       fill(255, 0, 0);
-       var player = players[index];
-       ellipse(player.position.x, player.position.y, player.mass, player.mass);
+       players[index].update();
+       players[index].draw();
    } 
 }
