d82a24c3c5d20a2b78cf3827f5e2798e98fe4ad5
Michel <michelschep@gmail.com>
2020-01-27 14:45:53 +0100

    Galaxy works
    more or less

--- sound_art/sound_art.js
+++ sound_art/sound_art.js
@@ -1,7 +1,7 @@
 let xoff = 0;
 let yoff = 1000000;
-let x = 1024/2;
-let y = 1024/2;
+let x = 1024 / 2;
+let y = 1024 / 2;
 let nn = 0;
 let cn = 0;
 
@@ -13,51 +13,58 @@ function setup() {
 }
 
 
-function draw() { 
-  //console.log(x, y);
+function draw() {
   micLevel = mic.getLevel();
-  
+  if (micLevel == 0)
+    return;
+
   var r = map(micLevel, 0, 0.8, 5, 100);
   if (r > 100) {
     r = 100;
   }
-  
+
   var n = noise(xoff);
-nn += n;
-cn += 1;
-var avg = nn/cn;
-  console.log(avg, n);
-  xoff += 0.001;
+
+  nn += micLevel;
+  cn += 1;
+  var avg = nn / cn;
+  console.log("AVG", avg, micLevel);
+
+  xoff += 0.01;
   var d1 = map(n, 0, 1, -0.9, 0.9);
-  x += (d1*r);
-  
+  x += (d1);
+
   n = noise(yoff);
-  yoff += 0.001;
+  yoff += 0.01;
   var d2 = map(n, 0, 1, -0.9, 0.9);
-  y += (d2*r);
- 
- //console.log(d1, d2);
- 
-  if (x<0) {
-   x=0;
+  y += (d2);
+
+  //console.log(d1, d2);
+
+  if (x < 0) {
+    xoff += 10;
+    x = 0;
   }
-  if (x>width) {
-   x = width;
+  if (x > width) {
+    xoff += 10;
+    x = width;
   }
-  if (y<0) {
-   y=0;
+  if (y < 0) {
+    yoff += 10;
+    y = 0;
   }
-  if (y>height) {
-   y = height;
+  if (y > height) {
+    yoff += 10;
+    y = height;
   }
-   
-  
-  var C = map(micLevel, 0, 0.0001, 0, 256*256*256);
-  
-  
+
+
+  var C = map(micLevel, 0, 0.0001, 0, 256 * 256 * 256);
+
+
   var B = C % 256;
-  var G = ((C-B)/256) % 256;
-  var R = ((C-B)/256*256) - G/256;
+  var G = ((C - B) / 256) % 256;
+  var R = ((C - B) / 256 * 256) - G / 256;
 
   stroke(R, G, B);
   ellipse(x, y, r, r);
--- spike_rankings_solar_system/spike_rankings_solar_system.js
+++ spike_rankings_solar_system/spike_rankings_solar_system.js
@@ -1,46 +1,88 @@
-let Player = function(position) {
-  this.position = position;
-  this.mass = 10;
-  this.velocity = createVector(0.03, 0.1);
+let players;
+let backhole;
 
-  this.update = function() {
-    this.position.add(this.velocity); 
+let Player = function (position, mass) {
+  this.position = position;
+  this.mass = mass;
+  this.velocity = createVector(0, 0.01);
+  this.acceleration = createVector(0, 0);
+
+  this.applyForce = function (force) {
+    this.acceleration.add(force);
   }
 
-  this.draw = function() {
-       fill(255, 0, 0);
-       ellipse(this.position.x, this.position.y, this.mass, this.mass);
+  this.update = function () {
+    if (mass == 3001)
+      return;
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+  }
+
+  this.draw = function () {
+    var c = map(this.mass, 0, 3000, 256, 0);
+    var r = map(this.mass, 0, 3000, 3, 100);
+    fill(c, c, c);
+    ellipse(this.position.x, this.position.y, r, r);
   }
 };
 
-
-let players;
 
 function preload() {
   //let url = 'file:///C:/data/github/Processing/spikes/spike_rankings_solar_system/players.json';
   //players = loadJSON(url);
 
+  blackhole = new Player(createVector(400, 400), 3001);
   //players = {list: [ { name: "Michel"}, { name: "Geale"} ]};
   //players = [ new Player(createVector(random(width), random(height)))];
-  var player1 = new Player(createVector(100, 100));
-  player1.mass = 50;
-
-  var player2 = new Player(createVector(200, 200));
-  player2.mass = 30;
-
-  players = [ player1, player2 ];
+  players = [
+    blackhole,
+    new Player(createVector(100, 100), 3000),
+    new Player(createVector(130, 130), 200),
+    new Player(createVector(132, 132), 2),
+    new Player(createVector(140, 130), 5),
+    new Player(createVector(700, 700), 200),
+    new Player(createVector(700, 100), 200),
+    new Player(createVector(100, 700), 200),
+    new Player(createVector(200, 10), 10),
+    new Player(createVector(280, 10), 500),
+    new Player(createVector(50, 400), 10)
+  ];
 }
 
 function setup() {
-    createCanvas(601, 600);
-    background(100);
+  createCanvas(800, 800);
+  background(100);
 }
 
 function draw() {
-   background(100);
-   for (var index=0; index<players.length; ++index) {
-       console.log(players[index].position);
-       players[index].update();
-       players[index].draw();
-   } 
+  var G = 0.00010;
+  background(100);
+//  translate(blackhole.position.x / 2, blackhole.position.y / 2);
+
+  for (var index = 0; index < players.length; ++index) {
+
+    var thisPlayer = players[index];
+
+    for (var other = 0; other < players.length; ++other) {
+      if (index == other)
+        continue;
+      var theOther = players[other];
+
+      var forceDirection = p5.Vector.sub(theOther.position, thisPlayer.position);
+      var magnitude = forceDirection.mag();
+      if (magnitude < 100)
+        continue;
+      var distance = magnitude;
+      forceDirection.normalize();
+      var strength = (G * thisPlayer.mass * theOther.mass) / (distance * distance);
+      forceDirection.mult(strength);
+
+      thisPlayer.applyForce(forceDirection);
+      //console.log(forceDirection);
+    }
+
+    players[index].update();
+    players[index].draw();
+  }
 }
