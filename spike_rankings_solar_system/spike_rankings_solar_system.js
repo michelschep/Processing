4955dfb5a5fbb84ed2b80d0c9de534f40c897619
Michel <michelschep@gmail.com>
2020-01-28 08:42:55 +0100

    Add slider to scale canvas

--- spike_rankings_solar_system/spike_rankings_solar_system.js
+++ spike_rankings_solar_system/spike_rankings_solar_system.js
@@ -1,5 +1,6 @@
 let players;
 let backhole;
+let slider;
 
 let Player = function (position, mass) {
   this.position = position;
@@ -12,17 +13,25 @@ let Player = function (position, mass) {
   }
 
   this.update = function () {
-    if (mass == 3001)
+    if (mass == 500001)
       return;
+
+    //if (this.velocity.mag() > 10) {
+    //  this.acceleration.mult(0);
+    //  return;
+    //}
+
     this.velocity.add(this.acceleration);
     this.position.add(this.velocity);
     this.acceleration.mult(0);
+    if (mass > 5000)
+      console.log(mass, this.velocity.mag(), this.position.x, this.position.y);
   }
 
   this.draw = function () {
-    var c = map(this.mass, 0, 3000, 256, 0);
-    var r = map(this.mass, 0, 3000, 3, 100);
-    fill(c, c, c);
+    var c = map(this.mass, 0, 500001, 256, 0);
+    var r = map(this.mass, 0, 500001, 3, 250);
+    fill(c, c, c, 100);
     ellipse(this.position.x, this.position.y, r, r);
   }
 };
@@ -32,16 +41,28 @@ function preload() {
   //let url = 'file:///C:/data/github/Processing/spikes/spike_rankings_solar_system/players.json';
   //players = loadJSON(url);
 
-  blackhole = new Player(createVector(400, 400), 3001);
+  blackhole = new Player(createVector(windowWidth/2, windowHeight/2), 500001);
   //players = {list: [ { name: "Michel"}, { name: "Geale"} ]};
   //players = [ new Player(createVector(random(width), random(height)))];
   players = [
     blackhole,
-    new Player(createVector(100, 100), 3000),
-    new Player(createVector(130, 130), 200),
+    new Player(createVector(500, 500), 20000),
+    new Player(createVector(500, 500), 10000),
+    new Player(createVector(1000, 100), 2800),
+    new Player(createVector(3000, 3000), 2400),
+    new Player(createVector(1000, 2000), 2000),
+    new Player(createVector(1000, 1000), 1500),
+    new Player(createVector(1000, 500), 1200),
+    new Player(createVector(130, 130), 1000),
     new Player(createVector(132, 132), 2),
     new Player(createVector(140, 130), 5),
-    new Player(createVector(700, 700), 200),
+    new Player(createVector(1000, 700), 800),
+    new Player(createVector(900, 600), 680),
+    new Player(createVector(800, 500), 560),
+    new Player(createVector(700, 400), 340),
+    new Player(createVector(600, 300), 220),
+    new Player(createVector(500, 200), 100),
+    new Player(createVector(400, 100), 80),
     new Player(createVector(700, 100), 200),
     new Player(createVector(100, 700), 200),
     new Player(createVector(200, 10), 10),
@@ -50,13 +71,24 @@ function preload() {
   ];
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight);
   background(100);
+
+  slider = createSlider(0, 1, 0.6, 0.001);
+  slider.position(10, 10);
+  slider.style('width', '500px');
 }
 
 function draw() {
-  var G = 0.00010;
+  let val = slider.value();
+  scale(val);
+  console.log(val);
+  var G = 0.00020;
   background(100);
 //  translate(blackhole.position.x / 2, blackhole.position.y / 2);
 
@@ -73,6 +105,7 @@ function draw() {
       var magnitude = forceDirection.mag();
       if (magnitude < 100)
         continue;
+
       var distance = magnitude;
       forceDirection.normalize();
       var strength = (G * thisPlayer.mass * theOther.mass) / (distance * distance);
