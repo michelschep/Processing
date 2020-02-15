e9ec3a18c39560b189c9df2236c084ce23695a42
Michel <michelschep@gmail.com>
2020-02-15 11:32:46 +0100

    Add code to create and show parabola

--- parabola/parabola.js
+++ parabola/parabola.js
@@ -2,18 +2,30 @@ var w = 1000;
 var h = 1000;
 function setup() {
   background(0);
-  createCanvas(1000, 1000, WEBGL);
-  strokeWeight(2);
-  
+  createCanvas(1000, 1000);
+
+  push();
+  strokeWeight(0.3);
+  translate(500, 500);
+  beginShape();
+  for(var x=-1.8 ;x<=1.8; x+=0.01) {
+    var y = x * x;
+    var px = map(x, -2, 2, -250, 250);
+    var py = map(y, 0, 4, 0, -500);
+    vertex(px,py);
+  }
+  endShape();
+  for(x=-1.8 ;x<=1.8; x+=0.1) {
+    var y = x * x;
+    var px = map(x, -2, 2, -250, 250);
+    var py = map(y, 0, 4, 0, -500);
+    fill(255,0,0);
+    ellipse(px, py, 4);
+  }
+  pop();
+  strokeWeight(0.04);
   line(w/2, 0, w/2, h);
   line(0, h/2, w, h/2);
-  
-  push();
-  translate(500, 500);
-  //rotateY(-1);
-  ellipse(20, 20, 5, 5);
-  pop();
-  
 }
 
 
