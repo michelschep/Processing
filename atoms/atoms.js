b818469d7a0989c72750da6afdc5974e8c73f44d
Michel <michelschep@gmail.com>
2020-10-06 16:23:56 +0200

    Picture 130

--- atoms/atoms.js
+++ atoms/atoms.js
@@ -2,36 +2,44 @@ var atoms = [];
 var index = 0;
 var w = 1024;
 var h = 960;
+var start = false;
+var fixedAtoms = 36;
 
 function Atom() {
-    if (index == 0)
+    if (index < 0)
         this.position = createVector(w/2, h/2);
-    else
-        this.position = createVector(w*random(1), h*random(1));
+    else {
+        var angle = random(1) * 2 * 3.14;
+        var r = random(5);
+        var x = r * cos(angle);
+        var y = r * sin(angle);
+
+        this.position = createVector(w/2, h/2).add(createVector(x, y));
+    }
 
     this.acceleration = createVector(0, 0);
     this.velocity = createVector(0, 0);
-    this.index = ++index;
+    this.index = index++;
     this.color = color(255*random(1), 255*random(1),255*random(1));
 
     this.update = function() {
-        if (this.index == 1)
+        if (this.index < fixedAtoms)
             return;
 
         atoms.forEach(element => {
             if (this.index != element.index) {
             var distance = dist(element.position.x, element.position.y, this.position.x, this.position.y) + 0.001;
-            var sigma = 100;
+            var sigma = 250;
             //console.log('position', element.position, element.position);
             //console.log('distance',distance);
             var forceMag = (-24/distance) * (Math.pow(sigma/distance, 12) - Math.pow(sigma/distance, 6));
             var force = p5.Vector.sub(element.position, this.position).normalize().mult(forceMag);
-            console.log(forceMag);
+            //console.log(forceMag);
             this.acceleration.add(force); 
             }
         });
 
-        this.velocity.add(this.acceleration).limit(5);
+        this.velocity.add(this.acceleration).limit(20);
         this.acceleration.mult(0);
         this.position.add(this.velocity);
         if (this.position.x < 0) {
@@ -51,11 +59,15 @@ function Atom() {
     this.show = function() {
         fill(this.color);
         noStroke();
-        ellipse(this.position.x, this.position.y, 5);
+        if (this.index < fixedAtoms)
+            ellipse(this.position.x, this.position.y, 0);
+        else
+            ellipse(this.position.x, this.position.y, 1);
+x
     }
 }
 
-function setup() {
+async function setup() {
     w = displayWidth;
     h = displayHeight;
     createCanvas(w, h);
@@ -64,6 +76,16 @@ function setup() {
     atoms = [];
     var center = createVector(w/2, h/2);
 
+    var angle = 0;
+    for (var i=0; i< fixedAtoms; ++i) {
+        var x = 450/16 * cos(angle);
+        var y = 350/16* sin(angle);
+        var a = new Atom();
+        a.position = createVector(w/2, h/2).add(createVector(x, y));
+        atoms.push(a);    
+        angle += 2*3.14/fixedAtoms;
+    }
+
     atoms.push(new Atom());
     atoms.push(new Atom());
     atoms.push(new Atom());
@@ -111,14 +133,65 @@ function setup() {
     atoms.push(new Atom());
     atoms.push(new Atom());
     atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+    atoms.push(new Atom());
+
+    start = true;
+    keyPressed();
 }
 
+async function keyPressed() {
+    await sleep(7000);
+    start = true;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 function draw() {
-    //background(125, 125, 125);
+    //background(0);
+ if (start) {
     atoms.forEach(element => {
         element.update();
     });
     atoms.forEach(element => {
         element.show();
     });
+}
 }
